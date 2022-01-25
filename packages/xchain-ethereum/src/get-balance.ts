5ebf90266982133c01e5dead345b6b063099f0e1
Jonny Burger <jonathanburger11@gmail.com>
2022-01-25 13:17:08 +0100

    make eth balances more performant

--- packages/xchain-ethereum/src/get-balance.ts
+++ packages/xchain-ethereum/src/get-balance.ts
@@ -29,6 +29,134 @@ const call = async <T>({
   return contract[func](...params)
 }
 
+const getMainnetBalances = async ({
+  ethplorerApiKey,
+  ethplorerUrl,
+  address,
+}: {
+  ethplorerUrl: string
+  ethplorerApiKey: string
+  address: Address
+}): Promise<Balances> => {
+  // use ethplorerAPI for mainnet - ignore assets
+  const account = await ethplorerAPI.getAddress(ethplorerUrl, address, ethplorerApiKey)
+
+  return getTokenBalances(account.tokens)
+}
+
+const getBalanceUnthrottled = async (
+  asset: Asset,
+  etherscanApiKey: string,
+  address: Address,
+  provider: ethers.providers.BaseProvider,
+): Promise<Balance | null> => {
+  const etherscan = new EtherscanProvider('testnet', etherscanApiKey)
+
+  if (assetToString(asset) === assetToString(AssetETH)) {
+    return null
+  }
+  // Handle token balances
+  const assetAddress = getTokenAddress(asset)
+  if (!assetAddress) {
+    throw new Error(`Invalid asset ${asset}`)
+  }
+  const balance = await etherscanAPI.getTokenBalance({
+    baseUrl: etherscan.baseUrl,
+    address,
+    assetAddress,
+    apiKey: etherscan.apiKey,
+  })
+  const decimals =
+    BigNumber.from(
+      await call<BigNumberish>({
+        contractAddress: assetAddress,
+        abi: erc20ABI,
+        func: 'decimals',
+        params: [],
+        provider,
+      }),
+    ).toNumber() || ETH_DECIMAL
+
+  if (Number.isNaN(decimals)) {
+    return null
+  }
+
+  return {
+    asset,
+    amount: baseAmount(balance.toString(), decimals),
+  }
+}
+
+export const getTestnetBalance = async ({
+  etherscanApiKey,
+  address,
+  provider,
+  assets,
+}: {
+  etherscanApiKey: string
+  address: Address
+  provider: ethers.providers.BaseProvider
+  assets: Asset[]
+}): Promise<Balances> => {
+  // use etherscan for testnet
+
+  // Follow approach is only for testnet
+  // For mainnet, we will use ethplorer api(one request only)
+  // https://github.com/xchainjs/xchainjs-lib/issues/252
+  // And to avoid etherscan api call limit, it gets balances in a sequence way, not in parallel
+
+  const throttle = pThrottle({
+    limit: 5,
+    interval: 1000,
+  })
+
+  const getBalance = throttle(getBalanceUnthrottled)
+
+  const balances = await Promise.all(assets.map((asset) => getBalance(asset, etherscanApiKey, address, provider)))
+
+  return balances.filter(Boolean)
+}
+
+const getEthBalance = async ({
+  provider,
+  address,
+}: {
+  provider: ethers.providers.Provider
+  address: Address
+}): Promise<Balance> => {
+  // get ETH balance directly from provider
+  const ethBalance: BigNumber = await provider.getBalance(address)
+  const ethBalanceAmount = baseAmount(ethBalance.toString(), ETH_DECIMAL)
+  return {
+    asset: AssetETH,
+    amount: ethBalanceAmount,
+  }
+}
+
+export const getTokenBalance = async ({
+  address,
+  network,
+  assets,
+  ethplorerUrl,
+  ethplorerApiKey,
+  etherscanApiKey,
+  provider,
+}: {
+  address: Address
+  network: Network
+  assets: Asset[]
+  ethplorerUrl: string
+  ethplorerApiKey: string
+  etherscanApiKey: string
+  provider: ethers.providers.BaseProvider
+}): Promise<Balances> => {
+  if (network === 'mainnet') {
+    return getMainnetBalances({ ethplorerApiKey, address, ethplorerUrl })
+  }
+
+  return getTestnetBalance({ etherscanApiKey, address, assets, provider })
+}
+
 export const getBalance = async ({
   address,
   network,
@@ -40,87 +168,23 @@ export const getBalance = async ({
 }: {
   address: Address
   network: Network
-  assets?: Asset[]
+  assets: Asset[]
   ethplorerUrl: string
   ethplorerApiKey: string
   etherscanApiKey: string
   provider: ethers.providers.BaseProvider
 }): Promise<Balances> => {
-  // get ETH balance directly from provider
-  const ethBalance: BigNumber = await provider.getBalance(address)
-  const ethBalanceAmount = baseAmount(ethBalance.toString(), ETH_DECIMAL)
-
-  if (network === 'mainnet') {
-    // use ethplorerAPI for mainnet - ignore assets
-    const account = await ethplorerAPI.getAddress(ethplorerUrl, address, ethplorerApiKey)
-    const balances: Balances = [
-      {
-        asset: AssetETH,
-        amount: ethBalanceAmount,
-      },
-    ]
-
-    if (account.tokens) {
-      balances.push(...getTokenBalances(account.tokens))
-    }
-
-    return balances
-  }
-  // use etherscan for testnet
-
-  const newAssets = assets || [AssetETH]
-  // Follow approach is only for testnet
-  // For mainnet, we will use ethplorer api(one request only)
-  // https://github.com/xchainjs/xchainjs-lib/issues/252
-  // And to avoid etherscan api call limit, it gets balances in a sequence way, not in parallel
-
-  const throttle = pThrottle({
-    limit: 5,
-    interval: 1000,
-  })
-
-  const getBalance = throttle(
-    async (asset: Asset): Promise<Balance> => {
-      const etherscan = new EtherscanProvider(network, etherscanApiKey)
-
-      if (assetToString(asset) !== assetToString(AssetETH)) {
-        // Handle token balances
-        const assetAddress = getTokenAddress(asset)
-        if (!assetAddress) {
-          throw new Error(`Invalid asset ${asset}`)
-        }
-        const balance = await etherscanAPI.getTokenBalance({
-          baseUrl: etherscan.baseUrl,
-          address,
-          assetAddress,
-          apiKey: etherscan.apiKey,
-        })
-        const decimals =
-          BigNumber.from(
-            await call<BigNumberish>({
-              contractAddress: assetAddress,
-              abi: erc20ABI,
-              func: 'decimals',
-              params: [],
-              provider,
-            }),
-          ).toNumber() || ETH_DECIMAL
-
-        if (!Number.isNaN(decimals)) {
-          return {
-            asset,
-            amount: baseAmount(balance.toString(), decimals),
-          }
-        }
-      }
-      return {
-        asset: AssetETH,
-        amount: ethBalanceAmount,
-      }
-    },
-  )
-
-  const balances = await Promise.all(newAssets.map((asset) => getBalance(asset)))
-
-  return balances
+  const [ethBalance, tokenBalances] = await Promise.all([
+    getEthBalance({ provider, address }),
+    getTokenBalance({
+      address,
+      assets,
+      etherscanApiKey,
+      ethplorerApiKey,
+      ethplorerUrl,
+      network,
+      provider,
+    }),
+  ])
+  return [ethBalance, ...tokenBalances]
 }
